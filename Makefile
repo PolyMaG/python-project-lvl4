f7e6d4de102f3dfe7316a25ac09500a748a2807a
PolyMag <maxxxlim@mail.ru>
2020-10-06 23:16:44 +0300

    add models tests

--- Makefile
+++ Makefile
@@ -1,16 +1,18 @@
 install:
 	poetry install
 
-init:
+loaddb:
 	poetry run python manage.py loaddata task_status_data.json
 
 lint:
 	poetry run flake8 task_manager tasks
-	# poetry run flake8 tests
+
+sort:
 	poetry run isort task_manager/*.py
-	# poetry run isort tests/*.py
+	poetry run isort tasks/tests/*.py
 
 test:
-	poetry run pytest -vv --cov=task_manager --cov-report xml tests/
+	poetry run python manage.py test
+	# poetry run pytest -vv --cov=task_manager --cov-report xml tests/
 
-.PHONY: install lint test publish
+.PHONY: install loaddb lint sort test 
---
+++ tasks/tests/test_models.py
@@ -0,0 +1,99 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+from tasks.models import Tag, Task, TaskStatus
+
+
+class TagModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified data for all class methods.
+        Tag.objects.create(title='New tag')
+
+    def test_title_label(self):
+        tag = Tag.objects.get(id=1)
+        field_label = tag._meta.get_field('title').verbose_name
+        self.assertEquals(field_label, 'title')
+
+    def test_title_max_length(self):
+        tag = Tag.objects.get(id=1)
+        max_length = tag._meta.get_field('title').max_length
+        self.assertEquals(max_length, 50)
+
+    def test_tag_name(self):
+        tag = Tag.objects.get(id=1)
+        tag_name = tag.title
+        self.assertEquals(tag_name, str(tag))
+
+    def test_absolute_url(self):
+        tag = Tag.objects.get(id=1)
+        self.assertEquals(tag.get_absolute_url(), '/tasks/tag/1/')
+
+
+class StatusModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified data for all class methods.
+        TaskStatus.objects.create(name='test')
+
+    def test_status_label(self):
+        status = TaskStatus.objects.get(id=1)
+        field_label = status._meta.get_field('name').verbose_name
+        self.assertEquals(field_label, 'name')
+
+    def test_status_max_length(self):
+        status = TaskStatus.objects.get(id=1)
+        max_length = status._meta.get_field('name').max_length
+        self.assertEquals(max_length, 4)
+
+    def test_status_name(self):
+        status = TaskStatus.objects.get(id=1)
+        status_name = status.name
+        self.assertEquals(status_name, str(status))
+
+
+class TaskModelTest(TestCase):
+    def setUp(self):
+        self.user1 = User.objects.create_user(
+            username='test1', password='12test12', email='test@example.com')
+        self.user2 = User.objects.create_user(
+            username='test2', password='12test12', email='test@example.com')
+        self.user1.save()
+        self.user2.save()
+        self.status = TaskStatus.objects.create(name='test')
+        self.status.save()
+        self.task = Task(name='TestTask',
+                         description='description',
+                         status=self.status,
+                         creator=self.user1,
+                         assigned_to=self.user2)
+        self.task.save()
+
+    def tearDown(self):
+        self.user1.delete()
+        self.user2.delete()
+
+    def test_name_max_length(self):
+        max_length = self.task._meta.get_field('name').max_length
+        self.assertEquals(max_length, 200)
+
+    def test_task_name(self):
+        self.assertEquals(self.task.name, str(self.task))
+
+    def test_absolute_url(self):
+        self.assertEquals(self.task.get_absolute_url(), '/tasks/task/1/')
+
+    def test_read_task(self):
+        self.assertEqual(self.task.status, self.status)
+        self.assertEqual(self.task.creator, self.user1)
+        self.assertEqual(self.task.assigned_to, self.user2)
+        self.assertEqual(self.task.description, 'description')
+
+    def test_update_task_(self):
+        self.task.description = 'new description'
+        self.task.assigned_to = self.user1
+        self.task.save()
+        self.assertEqual(self.task.description, 'new description')
+        self.assertEqual(self.task.assigned_to, self.user1)
