7ac161a9e9ace29bd11b4d5d3ed534556709f838
PolyMag <maxxxlim@mail.ru>
2020-12-05 22:34:05 +0300

    Update task_manager/views.py

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,6 +1,3 @@
-import sys
-
-import rollbar
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect, reverse
 from django.urls import reverse_lazy
@@ -8,14 +5,10 @@ from django.views import generic
 
 
 def redirect_tasks(request):
-    try:
-        return redirect(reverse('tasks:tasks_list_url'))
-    except:  # noqa E722
-        rollbar.report_exc_info(sys.exc_info())
+    return redirect(reverse("tasks:tasks_list_url"))
 
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html'
-# Create your views here.
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
