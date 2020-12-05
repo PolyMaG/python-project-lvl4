e0df0c82d0a53531dd82dd7d6b532cfd80228db1
PolyMag <maxxxlim@mail.ru>
2020-12-05 22:38:27 +0300

    Update auth tests

--- tasks/tests/test_auth.py
+++ tasks/tests/test_auth.py
@@ -12,7 +12,7 @@ class SigninTest(TestCase):
 
     def test_correct(self):
         user = authenticate(username='test', password='12test12')
-        self.assertTrue((user is not None) and user.is_authenticated)
+        self.assertTrue(user is not None and user.is_authenticated)
 
     def test_wrong_username(self):
         user = authenticate(username='wrong', password='12test12')
